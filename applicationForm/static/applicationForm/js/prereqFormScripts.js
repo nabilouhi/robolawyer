e41526a1640c97a2a12c0e7fc5cd72af30e3ae99
Aman <aman54kumar@gmail.com>
2019-11-04 17:33:53 +0100

    prereq page changes

--- applicationForm/static/applicationForm/js/prereqFormScripts.js
+++ applicationForm/static/applicationForm/js/prereqFormScripts.js
@@ -1,9 +1,9 @@
-$("input[name='concernYou']").change(function() {
-  result = this.value;
+// $("input[name='concernYou']").change(function() {
+//   result = this.value;
 
-  if (result === 'No') {
-    $('.concernOther').removeClass('is-hidden');
-  } else if (result === 'Yes') {
-    $('.concernOther').addClass('is-hidden');
-  }
-});
+//   if (result === 'No') {
+//     $('.concernOther').removeClass('is-hidden');
+//   } else if (result === 'Yes') {
+//     $('.concernOther').addClass('is-hidden');
+//   }
+// });
