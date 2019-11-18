9ef966762d9f6d769526cd9559be8ccf59fec7bd
Aman <aman54kumar@gmail.com>
2019-11-18 16:57:16 +0100

    updated db settings for prod

--- robolawyer/settings.py
+++ robolawyer/settings.py
@@ -84,14 +84,17 @@ WSGI_APPLICATION = 'robolawyer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    # 'default': dj_database_url.config()
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     # 'default': dj_database_url.config()
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {
+    'default': dj_database_url.config()
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
