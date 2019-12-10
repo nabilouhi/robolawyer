b05f491a1743e9c557278c1316ecf9ea11b42d13
admin <leliamm@uio.no>
2019-12-10 11:47:20 +0530

    added conditional db config statement for dev and prod

--- robolawyer/settings.py
+++ robolawyer/settings.py
@@ -83,22 +83,23 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'robolawyer.wsgi.application'
-
-
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.config()
+    }
+
 
 # DATABASES = {
 #     'default': dj_database_url.config()
