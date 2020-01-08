26fdc84e7dd00e9c45ca60e2ea7078fb6b4565ee
Kumar Aman <aman54kumar@gmail.com>
2020-01-08 10:01:43 +0530

    removed the code for weasyprint, code for making pdf from scratch

--- applicationForm/urls.py
+++ applicationForm/urls.py
@@ -3,10 +3,6 @@ from .views import FormPageView, formProcessing, feedback
 
 urlpatterns = [
     path('', FormPageView.as_view(), name='form'),
-    # path('formSubmit', submittedForm, name='formSubmit'),
     path('submit', formProcessing, name='formProcessing'),
     path('feedback', feedback, name="feedback")
-    # path('generate/', generatePDF, name='generate')
-    # path('', form, name='form'),
-    # path('/formSubmit', formSubmit, name='formSubmit'),
 ]
